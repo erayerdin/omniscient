4c451a2cea77007b23180f2deac533dab48c60ca
Eray Erdin <eraygezer.94@gmail.com>
2023-09-28 23:32:09 +0300

    customize MainNav

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -5,12 +5,53 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import { Card } from '@nextui-org/react'
+import Link from 'next/link'
+import React from 'react'
+import Cog from './icons/Cog'
+
+type NavEntryProps = {
+  icon?: React.ReactNode,
+  content: React.ReactNode,
+  linkTo: string,
+}
+
+const NavEntry = ({ icon, content, linkTo }: NavEntryProps) => {
+  return (
+    <Link href={linkTo} className='flex p-xs hover:bg-blue-500 hover:rounded-lg'>
+      {
+        icon != null 
+          ? <div className='mr-xs'>{icon}</div>
+          : undefined
+      }
+      <div>{content}</div>
+    </Link>
+  )
+}
 
 const MainNav = () => {
   return (
-  <Card className='p-xs h-full'>
-    <div>MainNav</div>
-  </Card>
+    <Card className='p-xs h-full'>
+      <NavEntry
+        icon={<Cog />}
+        content={<div>Overview</div>}
+        linkTo='/'
+      />
+      <NavEntry
+        icon={<Cog />}
+        content={<div>Overview</div>}
+        linkTo='/'
+      />
+      <NavEntry
+        icon={<Cog />}
+        content={<div>Overview</div>}
+        linkTo='/'
+      />
+      <NavEntry
+        icon={<Cog />}
+        content={<div>Overview</div>}
+        linkTo='/'
+      />
+    </Card>
   )
 }
 
