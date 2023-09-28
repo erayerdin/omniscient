b47fbd44362938daf042a51ec6386869aacba820
Eray Erdin <eraygezer.94@gmail.com>
2023-09-28 19:10:31 +0300

    change into client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-import Image from 'next/image'
+"use client";
+
+import Image from 'next/image';
+
 
 export default function Home() {
   return (
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+"use client";
+
 import { NextUIProvider } from "@nextui-org/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
