c744089f732dafda32abe6801767d81ef59c2c42
Eray Erdin <eraygezer.94@gmail.com>
2023-09-28 19:29:49 +0300

    set responsiveness for large screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ import MainNav from "./components/MainNav";
 export default function Home() {
   return (
     <div className="container flex h-screen">
-      <div className="w-2/5 m-sm">
+      <div className="w-2/5 lg:w-1/4 m-sm">
         <MainNav />
       </div>
-      <div className="w-3/5 m-sm">
+      <div className="w-3/5 lg:w-3/4 m-sm">
         <div>Content</div>
       </div>
     </div>
