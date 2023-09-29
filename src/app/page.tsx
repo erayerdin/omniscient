f1ac74bc2e3e7aa36354c2b436d9d86e4abbf6fb
Eray Erdin <eraygezer.94@gmail.com>
2023-09-29 14:11:59 +0300

    add cpu info modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { Card, CircularProgress } from "@nextui-org/react";
-import { useOverviewInfo } from "./hooks";
+import { Card, CircularProgress, Modal, ModalBody, ModalContent, ModalHeader, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, useDisclosure } from "@nextui-org/react";
+import { useCpuInfo, useOverviewInfo } from "./hooks";
 
 type ResourceCardProps = {
   label: string,
   used: number,
   total: number,
   footer: (used: number, total: number) => React.ReactNode,
+  onPress?: () => void,
 }
 
-const ResourceCard = ({ label, used, total, footer }: ResourceCardProps) => {
+const ResourceCard = ({ label, used, total, footer, onPress }: ResourceCardProps) => {
   const value = used / total;
   const valuePercent = value * 100;
 
@@ -25,7 +26,7 @@ const ResourceCard = ({ label, used, total, footer }: ResourceCardProps) => {
   }
 
   return (
-    <Card className="flex p-sm items-center space-y-2 w-64">
+    <Card onPress={onPress} isPressable={onPress !== null} className="flex p-sm items-center space-y-2 w-64">
       <header className="text-xl">{label}</header>
       <CircularProgress
         size="lg"
@@ -41,8 +42,56 @@ const ResourceCard = ({ label, used, total, footer }: ResourceCardProps) => {
   );
 }
 
+const CpuModal = ({ isOpen, onOpenChange }: { isOpen: boolean, onOpenChange: () => void }) => {
+  const cpus = useCpuInfo();
+  console.log(cpus);
+
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+      <ModalContent>
+        {(onClose) => {
+          return (
+            <>
+              <ModalHeader className="flex flex-col gap-1 items-center">
+                CPU Information
+              </ModalHeader>
+              <ModalBody className="flex items-center">
+                {
+                  cpus.length === 0
+                    ? <CircularProgress aria-label="Loading CPU information..." />
+                    : (
+                      <Table>
+                        <TableHeader>
+                          <TableColumn>Name</TableColumn>
+                          <TableColumn>Vendor ID</TableColumn>
+                          <TableColumn>Brand</TableColumn>
+                          <TableColumn>Frequency</TableColumn>
+                        </TableHeader>
+                        <TableBody>
+                          {cpus.map((cpu) => (
+                            <TableRow key={cpu.name}>
+                              <TableCell>{cpu.name}</TableCell>
+                              <TableCell>{cpu.vendorId}</TableCell>
+                              <TableCell>{cpu.brand}</TableCell>
+                              <TableCell>{cpu.frequency} MHz</TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    )
+                }
+              </ModalBody>
+            </>
+          )
+        }}
+      </ModalContent>
+    </Modal>
+  );
+}
+
 export default function Home() {
   const overviewInfo = useOverviewInfo();
+  const { isOpen: isCpuModalOpen, onOpen: onCpuModalOpen, onOpenChange: onCpuModalOpenChange } = useDisclosure();
 
   return (
     <div className="flex">
@@ -53,6 +102,7 @@ export default function Home() {
           used={overviewInfo.currentCpuUsage}
           total={100}
           footer={(used, total) => <div>{(used / total * 100).toFixed(2)}%</div>}
+          onPress={onCpuModalOpen}
         />
         <ResourceCard
           label="Memory"
@@ -67,6 +117,9 @@ export default function Home() {
           footer={(used, total) => <div>{used.toFixed(2)} / {total.toFixed(2)} GiB</div>}
         />
       </header>
+
+      {/** Modals */}
+      <CpuModal isOpen={isCpuModalOpen} onOpenChange={onCpuModalOpenChange} />
     </div>
   )
 }
