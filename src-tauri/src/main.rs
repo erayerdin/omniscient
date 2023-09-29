31363e49fc0cf501f0beff8df5e36ed8b3221416
Eray Erdin <eraygezer.94@gmail.com>
2023-09-29 05:42:35 +0300

    manage SystemState

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,18 +7,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::sync::Mutex;
-
 use app::{cli::Cli, commands::cpu::*, logging::init_logger};
 use clap::Parser;
-use sysinfo::{System, SystemExt};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::try_parse()?;
     init_logger(cli)?;
 
     tauri::Builder::default()
-        .manage(Mutex::new(System::new()))
+        .manage(SystemState::default())
         .invoke_handler(tauri::generate_handler![get_cpu_usage])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
