6648287c51a435ee4bbdac998692cc76825cfd1d
Eray Erdin <eraygezer.94@gmail.com>
2023-10-01 12:40:25 +0300

    remove redundant import

--- src-tauri/src/commands/cpu.rs
+++ src-tauri/src/commands/cpu.rs
@@ -6,11 +6,7 @@
 
 use sysinfo::{CpuExt, SystemExt};
 
-use crate::{
-    models::{cpu::Cpu, disk::Disk},
-    states::system::SystemState,
-    OmniscientError,
-};
+use crate::{models::cpu::Cpu, states::system::SystemState, OmniscientError};
 
 #[tauri::command]
 pub fn get_cpu_usage(system_state: tauri::State<SystemState>) -> Result<f32, OmniscientError> {
