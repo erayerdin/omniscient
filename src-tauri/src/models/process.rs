016b323c5a96a03606b8f9b10f74d0146e220e5c
Eray Erdin <eraygezer.94@gmail.com>
2023-10-01 17:26:01 +0300

    add Process type

--- src-tauri/src/models/process.rs
+++ src-tauri/src/models/process.rs
@@ -4,7 +4,15 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-pub mod cpu;
-pub mod disk;
-pub mod metadata;
-pub mod process;
+use std::path;
+
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Process {
+    pid: u64,
+    path: path::PathBuf,
+    cpu_usage: u64,
+    memory_usage: u64,
+}
