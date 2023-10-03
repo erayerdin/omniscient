73d43596c3081175ff9e34ba6ee7c6daedf7ab0f
Eray Erdin <eraygezer.94@gmail.com>
2023-10-03 15:05:26 +0300

    fill remaining width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,11 +22,11 @@ export default function RootLayout({
     <html lang="en" className='dark' onContextMenu={(e) => e.preventDefault()}>
       <body className={inter.className}>
         <Providers>
-          <div className="container flex h-screen">
-            <div className="w-2/5 lg:w-1/4 m-sm">
+          <div className="flex h-screen">
+            <div className="w-2/5 sm:w-1/4 m-sm">
               <MainNav />
             </div>
-            <div className="w-3/5 lg:w-3/4 m-sm overflow-scroll">
+            <div className="flex-grow m-sm overflow-scroll">
               {children}
             </div>
           </div>
