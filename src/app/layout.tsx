42c7ccd26a916bc12ba688b6e6085b6ba467082c
Eray Erdin <eraygezer.94@gmail.com>
2023-10-03 14:55:35 +0300

    add scroll for overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
             <div className="w-2/5 lg:w-1/4 m-sm">
               <MainNav />
             </div>
-            <div className="w-3/5 lg:w-3/4 m-sm">
+            <div className="w-3/5 lg:w-3/4 m-sm overflow-scroll">
               {children}
             </div>
           </div>
