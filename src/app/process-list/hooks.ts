09493ae7324e766f8f5a22a5e61a7b19c10b5b59
Eray Erdin <eraygezer.94@gmail.com>
2023-10-05 11:59:43 +0300

    change hooks to client

--- src/app/process-list/hooks.ts
+++ src/app/process-list/hooks.ts
@@ -4,6 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+"use client";
+
 import { SortDescriptor } from "@nextui-org/react";
 import { invoke } from "@tauri-apps/api";
 import { useEffect, useState } from "react";
