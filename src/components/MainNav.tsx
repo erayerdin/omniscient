4d064eba981f77117b0c2eb5e48f7f246a745cd5
Eray Erdin <eraygezer.94@gmail.com>
2023-10-05 11:51:03 +0300

    change MainNav to client component

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -4,12 +4,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { Card } from '@nextui-org/react'
-import Link from 'next/link'
-import React from 'react'
-import Cog from './icons/Cog'
-import ListBullet from './icons/ListBullet'
-import QuestionMarkCircle from './icons/QuestionMarkCircle'
+"use client";
+
+import { Card } from '@nextui-org/react';
+import Link from 'next/link';
+import React from 'react';
+import Cog from './icons/Cog';
+import ListBullet from './icons/ListBullet';
+import QuestionMarkCircle from './icons/QuestionMarkCircle';
 
 type NavEntryProps = {
   icon?: React.ReactNode,
